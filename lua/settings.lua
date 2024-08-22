0ebf46d7ecbbed1217a80203ec44c345b91d06b1
falamous <falamous@mail.ru>
2024-08-22 19:36:13 +0300

    add indent visual fixes

--- lua/settings.lua
+++ lua/settings.lua
@@ -2,8 +2,11 @@
 vim.opt.mouse = "nvi" -- Actually use the mouse
 vim.opt.ignorecase = true -- Make searching case insensitive
 vim.opt.smartcase = true -- ... unless the query has capital letters.
-vim.opt.gdefault = true -- Use 'g' flag by default with :s/foo/bar/.
+vim.opt.gdefault = true -- Use 'g' flag by default with :s/foo/bar/
 vim.opt.undofile = true
 vim.opt.signcolumn = "number"
 vim.opt.relativenumber = true
 vim.opt.number = true
+vim.opt.splitbelow = true
+vim.keymap.set("v", ">", ">gv", { noremap = true, desc = "Indent right" })
+vim.keymap.set("v", "<", "<gv", { noremap = true, desc = "Indent left" })
